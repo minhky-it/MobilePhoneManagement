b008fc6d31a92e279a7f3c74d35ddea335272b1b
Minh Kỳ <minhky.book@gmail.com>
2023-05-05 19:55:36 +0700

    new edit

--- WebMVC_OrderForm/Models/MOBILEMANAGEMENT.Designer.cs
+++ WebMVC_OrderForm/Models/MOBILEMANAGEMENT.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\PC\Desktop\SE_50%\MobilePhoneManagement\WebMVC_OrderForm\Models\MOBILEMANAGEMENT.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\MobilePhoneManagement\WebMVC_OrderForm\Models\MOBILEMANAGEMENT.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
