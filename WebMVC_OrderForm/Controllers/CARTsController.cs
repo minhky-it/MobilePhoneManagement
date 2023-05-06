b8a02eb711eb2f3125f887ee710f9234fa0ad6db
Minh Kỳ <minhky.book@gmail.com>
2023-05-06 10:05:06 +0700

    new update

--- WebMVC_OrderForm/Controllers/CARTsController.cs
+++ WebMVC_OrderForm/Controllers/CARTsController.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
 using WebMVC_OrderForm.Models;
+using System.Net.Mail;
 
 namespace WebMVC_OrderForm.Controllers
 {
@@ -134,7 +136,168 @@ namespace WebMVC_OrderForm.Controllers
         }
         public ActionResult OrderConfirm()
         {
-            return View("Confirm");
+            var cARTs = db.CARTs.Include(c => c.Customer).Include(c => c.Product);
+            return View(cARTs);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> MakeOrder(FormCollection form)
+        {
+            string fullName = form["fullname"];
+            string email = form["email"];
+            string address = form["address"];
+            string phone = form["phone"];
+            double total = Convert.ToDouble(form["total"]);
+            var cARTs = db.CARTs.Include(c => c.Customer).Include(c => c.Product).ToList();
+
+            
+            // Use the value of the full name input field as needed
+
+            // generates a random number between 1000 and 9999
+            Random random = new Random();
+            DateTime currentDateTime = DateTime.Now;
+            Bill bill = new Bill();
+            bill.billID = random.Next(1000, 999999).ToString();
+            bill.total = total;
+            bill.createDate = currentDateTime;
+            bill.address = address;
+            bill.phone = phone;
+            bill.email = email;
+            bill.fullname = fullName;
+            if (ModelState.IsValid)
+            {
+                db.Bills.Add(bill);
+                await db.SaveChangesAsync();
+            }
+
+            foreach (var item in cARTs)
+            {
+                DetailReceipt detail = new DetailReceipt
+                {
+                    billID = bill.billID,
+                    productID = item.productID,
+                    quantity = item.quantity
+                };
+
+                db.DetailReceipts.Add(detail);
+                await db.SaveChangesAsync();
+            }
+            db.CARTs.RemoveRange(db.CARTs);
+            db.SaveChanges();
+
+            //Prepair email
+            string body = $@"<!DOCTYPE html>
+            <html>
+            <head>
+                <title>Order Confirmation</title>
+                <style>
+                    body {{
+                        font-family: Arial, sans-serif;
+                        font-size: 16px;
+                        line-height: 1.5;
+                        background-color: #f4f4f4;
+                    }}
+                    .container {{
+                        max-width: 600px;
+                        margin: 0 auto;
+                        padding: 20px;
+                        background-color: #fff;
+                        border-radius: 10px;
+                        box-shadow: 0px 0px 10px rgba(0,0,0,0.2);
+                    }}
+                    h1 {{
+                        font-size: 24px;
+                        margin-bottom: 20px;
+                        text-align: center;
+                    }}
+                    table {{
+                        width: 100%;
+                        border-collapse: collapse;
+                        margin-bottom: 20px;
+                    }}
+                    th, td {{
+                        padding: 10px;
+                        text-align: left;
+                        border-bottom: 1px solid #ddd;
+                    }}
+                    th {{
+                        background-color: #f2f2f2;
+                    }}
+                    .bold {{
+                        font-weight: bold;
+                    }}
+                    .text-right {{
+                        text-align: right;
+                    }}
+                    .thanks {{
+                        font-size: 18px;
+                        font-style: italic;
+                        margin-top: 20px;
+                        text-align: center;
+                    }}
+                </style>
+            </head>
+            <body>
+                <div class=""container"">
+                    <h1>Order Confirmation</h1>
+                    <table>
+                        <tr>
+                            <td class=""bold"">Order ID:</td>
+                            <td>{bill.billID}</td>
+                        </tr>
+                        <tr>
+                            <td class=""bold"">Order Date:</td>
+                            <td>{bill.createDate}</td>
+                        </tr>
+                        <tr>
+                            <td class=""bold"">Full Name:</td>
+                            <td>{bill.fullname}</td>
+                        </tr>
+                        <tr>
+                            <td class=""bold"">Email:</td>
+                            <td>{bill.email}</td>
+                        </tr>
+                        <tr>
+                            <td class=""bold"">Phone:</td>
+                            <td>{bill.phone}</td>
+                        </tr>
+                        <tr>
+                            <td class=""bold"">Address:</td>
+                            <td>{bill.address}</td>
+                        </tr>
+                        <tr>
+                            <td class=""bold"">Total:</td>
+                            <td class=""text-right"">{bill.total}</td>
+                        </tr>
+                    </table>
+                    <p class=""thanks"">Thank you for your order!</p>
+                </div>
+            </body>
+            </html>";
+            MailMessage message = new MailMessage();
+            message.Body = body;
+            //Send email
+            // Create a new MailMessage object
+            message.From = new MailAddress("minhky.book@gmail.com"); // Replace with your own email address
+            message.To.Add(new MailAddress(email)); // Replace with the recipient email address
+            message.Subject = "ORDER " + bill.billID + " Notification";
+            message.IsBodyHtml = true;
+            
+
+            // Create a new SmtpClient object and set the Gmail SMTP server details
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.UseDefaultCredentials = true;
+            client.Credentials = new NetworkCredential("kkmobile.test.mvc@gmail.com", "hzkkshmhlrcabykd");
+            client.EnableSsl = true;
+            // Send the email
+            client.Send(message);
+
+            return RedirectToAction("Succeed");
+        }
+
+        public ActionResult Succeed()
+        {
+            return View("Succeed");
         }
     }
 }
